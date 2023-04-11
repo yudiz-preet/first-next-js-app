4d1e58e0059d71aeb28597c03c01d339274b897e
Preet Jasani <preet.jasani@yudiz.com>
2023-04-11 15:09:40 +0530

    nested routing

---
+++ pages/cricket/about.js
@@ -0,0 +1,7 @@
+import React from "react";  
+
+const About = () => {
+    return (<>Checking about.js file</>)
+}
+
+export default About;
---
+++ pages/cricket/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Cricket = () => {
+    return (<>This is Cricket index.js file</>)
+}
+
+export default Cricket;
